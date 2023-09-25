7a733d1270aec1a3e6456225d1b40a2a66459e3d
nnrwp <rawipa_s@cmu.ac.th>
2023-09-25 16:30:12 +0700

    completed

--- src/app/api/enrollment/route.js
+++ src/app/api/enrollment/route.js
@@ -81,32 +81,46 @@ export const POST = async (request) => {
   }
 
   const prisma = getPrisma();
+
   //1.check if courseNo does not exist on database
-  //send this response back if courseNo does not exist
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Course number does not exist",
-  //   },
-  //   { status: 400 }
-  // );
+  const courseNoExists = await prisma.course.findUnique({
+    where: {
+      courseNo: courseNo,
+    },
+  });
+  if (!courseNoExists) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Course number does not exist",
+      },
+      { status: 400 }
+    );
+  }
 
   //2.check if such student enroll that course already (both "studentId" and "courseNo" exists on enrollment collection)
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "You already registered this course",
-  //   },
-  //   { status: 400 }
-  // );
-
+  const enrollmentExists = await prisma.enrollment.findFirst({
+    where: {
+      studentId: studentId,
+      courseNo: courseNo,
+    },
+  });
+  if (enrollmentExists) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "You already registered this course",
+      },
+      { status: 400 }
+    );
+  }
   //3.if conditions above are not met, perform inserting data here
-  // await prisma.enrollment.create({
-  //   data:{
-  //     ...
-  //   }
-  // })
-
+  await prisma.enrollment.create({
+    data: {
+      studentId: studentId,
+      courseNo: courseNo,
+    },
+  });
   return NextResponse.json({
     ok: true,
     message: "You has enrolled a course successfully",
